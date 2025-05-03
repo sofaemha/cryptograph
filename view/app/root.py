4f763869a3944de96612fa7ccdbd5c339183eaa3
sofaemha <mail@sofa.my.id>
2025-05-03 09:45:55 +0000

    feat: Implement basic UI for file encryption/decryption
    This commit introduces the initial user interface for the application.
    It includes:
    
    - Application title update to "AES File Locker".
    - UI layout using PanedWindow.
    - Encryption and decryption buttons.
    - System settings, about, and exit buttons.
    - Copyright label at the bottom.
    - Disabling window resizing for a fixed layout.

--- view/app/root.py
+++ view/app/root.py
@@ -1,18 +1,34 @@
-from system.py.tk import tkt
+from system.py.core import date
+from system.py.tk import tk, tkt
 
 
 class App(tkt.ThemedTKinterFrame):
     def __init__(self, theme, mode, usecommandlineargs=True, usethemeconfigfile=True):
         super().__init__(
-            "AES-LF",
+            "AES File Locker",
             theme,
             mode,
             usecommandlineargs=usecommandlineargs,
             useconfigfile=usethemeconfigfile,
         )
+        self.root.resizable(False, False)
 
-        self.frame = self.addLabelFrame("Main Menu")
-        self.frame.AccentButton("Encrypt", command=self.debugPrint)
+        self.panedWindow = self.PanedWindow("Paned Window Test")
 
-        self.debugPrint()
+        self.paneTop = self.panedWindow.addWindow()
+        self.paneTop.Label("AES File Locker", colspan=2)
+
+        self.menuFrame = self.paneTop.addLabelFrame("Data")
+        self.menuFrame.AccentButton("Encrypt", command=self.debugPrint)
+        self.menuFrame.AccentButton("Decrypt", command=self.debugPrint, col=1)
+        self.menuFrame.AccentButton("Clear Data", command=self.debugPrint, colspan=2)
+
+        self.systemFrame = self.paneTop.addLabelFrame("System")
+        self.systemFrame.AccentButton("Setting", command=self.debugPrint)
+        self.systemFrame.AccentButton("About", command=self.debugPrint, col=1)
+        self.systemFrame.AccentButton("Exit", command=self.handleExit, colspan=2)
+
+        self.paneBottom = self.panedWindow.addWindow()
+        self.paneBottom.Label(f"All Rights Reserved \u00A9 {date.now().year}", size=10, weight="normal")
+
         self.run()
