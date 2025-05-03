6ae3e9a2bcea5e6b010e5859191b60ec35a16ba8
sofaemha <mail@sofa.my.id>
2025-05-03 07:16:52 +0000

    feat: Implement application setup and start logic

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
 from system.py.initialization import *
+from system.py.library import library
+
+
+class App:
+    @staticmethod
+    def setup():
+        # ! Module and function must be same order
+        module = [
+            "tkinter",
+            "Crypto",
+            "TKinterModernThemes",
+        ]  # Modules name stored in `sys.modules`
+        fn = [
+            "tkinter",
+            "pycryptodome",
+            "tkintertheme",
+        ]  # Functions to call on `system.library`
+        status = library.check(module, fn)
+        if not status:
+            log.write("Some libraries are missing")
+            system.exit(1)
+
+    def start(self):
+        print("Starting the application...")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.setup()
+    app.start()
