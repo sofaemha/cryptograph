6ee450a090f369312e7f6411f153e377666a4738
sofaemha <mail@sofa.my.id>
2025-05-07 10:55:35 +0000

    feat: init Initialize public folder and log file.

--- system/py/initialization.py
+++ system/py/initialization.py
@@ -3,6 +3,12 @@ from dotenv import load_dotenv as load_env
 load_env()
 
 from system.py.core import *
+
+FOLDER_PUBLIC = env("FOLDER_PUBLIC")
+FILE_LOG = env("FILE_LOG")
+os.makedirs("public", exist_ok=True)
+open(f"{FOLDER_PUBLIC}{FILE_LOG}", "a").close()
+
 from system.controller.file import file
 from system.controller.log import log
 from system.controller.folder import folder
