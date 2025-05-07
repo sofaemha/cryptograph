971ddcb96a10b67707966813a4e533eb5d77aeb8
sofaemha <mail@sofa.my.id>
2025-05-07 05:47:04 +0000

    feat(tk.py): Implement file import functionality

--- system/py/tk.py
+++ system/py/tk.py
@@ -1,2 +1,19 @@
+from system.py.core import env, fs
 import tkinter as tk
 import TKinterModernThemes as tkt
+from tkinter import filedialog, messagebox as alert
+
+PATH_ENCRYPT = env("PATH_ENCRYPT")
+
+def getfile():
+    file_path = filedialog.askopenfilename(
+        filetypes=[("Text Files", "*.txt")],
+        title="Select a Text File",
+    )
+    if file_path:
+        filename = file_path.split("/")[-1]
+        fs.copy(file_path, PATH_ENCRYPT)
+        alert.showinfo("System", f"File `{filename}` successfully imported into `{PATH_ENCRYPT}`.")
+        return filename
+    else:
+        alert.showwarning("System", "Please select a text (.txt) file!")
