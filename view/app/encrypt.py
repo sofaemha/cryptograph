593461e166a9d125a5872001b1aa345577eb5d5a
sofaemha <mail@sofa.my.id>
2025-05-07 10:55:27 +0000

    feat: Refactor encrypt.py for improved UI and encryption
    This commit refactors the encrypt.py file to enhance the user interface and incorporate a dedicated cryptography module for file encryption, improving code organization and maintainability.

--- view/app/encrypt.py
+++ view/app/encrypt.py
@@ -1,13 +1,12 @@
-from system.library.pycryptodome import aes, random_bytes
 from system.py.core import date, env
 from system.controller.log import log
 from system.controller.file import file
+from system.py.crypt import crypt
 from system.py.tk import tk, tkt, getfile, alert
 from system.controller.folder import folder
 
 PATH_ENCRYPT = env("PATH_ENCRYPT")
 PATH_DECRYPT = env("PATH_DECRYPT")
-PATH_KEY = env("PATH_KEY")
 
 
 class App(tkt.ThemedTKinterFrame):
@@ -20,49 +19,63 @@ class App(tkt.ThemedTKinterFrame):
             useconfigfile=usethemeconfigfile,
         )
 
-        self.observer = None
-        self.treeView = None
-        self.treeData = None
-        self.treeFolder = None
-        self.treeRawFolder = None
-        self.folder = folder
-        self.getfile = getfile
+        self.panedWindow = self.PanedWindow("Decrypt")
+        self.root.resizable(False, False)
         self.usecommandargs = tk.BooleanVar(value=True)
         self.usethemeconfigfile = tk.BooleanVar(value=True)
+        self.folder = folder
+        self.getfile = getfile
 
-        self.root.resizable(False, False)
+        self.paneTop = None
+        self.treeFrame = None
+        self.treeRawFolder = None
+        self.treeFolder = None
+        self.treeData = None
+        self.treeView = None
+        self.encryptFrame = None
+        self.systemFrame = None
+        self.paneBottom = None
 
-        self.panedWindow = self.PanedWindow("Paned Window Test")
+        self.pane_top()
+        self.tree_frame()
+        self.encrypt_frame()
+        self.system_frame()
+        self.pane_bottom()
 
+        self.run()
+
+    def pane_top(self):
         self.paneTop = self.panedWindow.addWindow()
-        self.paneTop.Label("AES File Locker", colspan=2)
+        self.paneTop.Label("AES-FL : Encrypt", colspan=2)
 
+    def pane_bottom(self):
+        self.paneBottom = self.panedWindow.addWindow()
+        self.paneBottom.Label(f"All Rights Reserved \u00A9 {date.now().year}", size=10, weight="normal")
+
+    def encrypt_frame(self):
+        self.encryptFrame = self.paneTop.addLabelFrame("Action", col=1)
+        self.encryptFrame.AccentButton("Encrypt", command=self.tree_encrypt, colspan=2)
+        self.encryptFrame.AccentButton("Delete", command=self.tree_delete, col=1, row=1)
+        self.encryptFrame.AccentButton("Import", command=self.tree_import, row=1)
+
+    def system_frame(self):
+        self.systemFrame = self.paneTop.addLabelFrame("System", col=1)
+        self.systemFrame.AccentButton("Kembali", self.handleExit, widgetkwargs={"width": 15})
+
+    def tree_frame(self):
         self.treeFrame = self.paneTop.addLabelFrame("Data", rowspan=2)
         self.treeRawFolder = [f'{PATH_ENCRYPT}{filename}' for filename in self.folder.list(PATH_ENCRYPT)]
         self.treeFolder = [
             [f, file.timestamp(f"{"/".join(f.split("/")[:-1])}/", f.split("/")[-1])] for f in self.treeRawFolder
         ]
         self.treeData = [
-            {"path": "public/encrypt/", "encrypt": f[0].split("/")[-1], "date": f[1]} for f in self.treeFolder
+            {"encrypt": f[0].split("/")[-1], "date": f[1]} for f in self.treeFolder
         ]
-        self.treeView = self.treeFrame.Treeview(["Encrypt", "Date"], [120, 120], 5, self.treeData, None,
+        self.treeView = self.treeFrame.Treeview(["Encrypt", "Date"], [150, 150], 5, self.treeData, None,
                                                 ["encrypt", "date"])
 
-        self.encryptFrame = self.paneTop.addLabelFrame("Enkripsi", col=1)
-        self.encryptFrame.AccentButton("Encrypt", command=self.tree_encrypt, colspan=2)
-        self.encryptFrame.AccentButton("Delete", command=self.tree_delete, col=1, row=1)
-        self.encryptFrame.AccentButton("Import", command=self.tree_import, row=1)
-
-        self.systemFrame = self.paneTop.addLabelFrame("System", col=1)
-        self.systemFrame.AccentButton("Kembali", self.handleExit, widgetkwargs={"width": 15})
-
-        self.paneBottom = self.panedWindow.addWindow()
-        self.paneBottom.Label(f"All Rights Reserved \u00A9 {date.now().year}", size=10, weight="normal")
-
-        self.run()
-
     def tree_select(self):
-        data = [{**self.treeView.item(index), "path": self.treeData[0]["path"], "index": index} for index in
+        data = [{**self.treeView.item(index), "path": PATH_ENCRYPT, "index": index} for index in
                 self.treeView.selection()]
 
         return None if not data else data
@@ -83,34 +96,15 @@ class App(tkt.ThemedTKinterFrame):
             confirm = "no"
 
         if confirm == "yes":
-            for item in data:
-                try:
-                    filename = item["text"].split(".")[0]
-                    text = file.read(item["path"], item["text"], binary=True)
-                    key = random_bytes(16)
-                    file.write(PATH_KEY, f"{filename}.key.txt", key, binary=True)
-                    cipher = aes.new(key, aes.MODE_OCB)
-                    ciphertext, tag = cipher.encrypt_and_digest(text)
-                    assert len(cipher.nonce) == 15
-
-                    f = file.write(PATH_DECRYPT, f"{filename}.enc", close=False, binary=True)
-                    f.write(tag)
-                    f.write(cipher.nonce)
-                    f.write(ciphertext)
-                    f.write(key)
-                    f.close()
-
-                except Exception as e:
-                    log.write(f"Error encrypting file `{item['text']}`: {e}")
-                    alert.showerror("System", f"An error occurred while encrypting `{item['text']}`: {e}")
-
+            crypt.encrypt(data)
             alert.showinfo("System", "File encryption completed successfully.")
 
     def tree_import(self):
         try:
             filename = self.getfile()
-            self.treeView.insert("", "end", text=filename, values=[file.timestamp(PATH_ENCRYPT, filename)])
-            alert.showinfo("System", f"File `{filename}` successfully added to tree view.")
+            if filename:
+                self.treeView.insert("", "end", text=filename, values=[file.timestamp(PATH_ENCRYPT, filename)])
+                alert.showinfo("System", f"File `{filename}` successfully added to tree view.")
         except Exception as e:
             log.write(f"Error importing file: {e}")
             alert.showerror("System", f"An error occurred while importing the file: {e}")
