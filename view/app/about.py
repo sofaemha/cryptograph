8200d2e4aecd7e7b986c6fbd24c492bf50897396
sofaemha <mail@sofa.my.id>
2025-05-07 05:46:31 +0000

    feat: Add about page

---
+++ view/app/about.py
@@ -0,0 +1,48 @@
+from system.py.core import date
+from system.py.tk import tk, tkt
+
+
+class App(tkt.ThemedTKinterFrame):
+    def __init__(self, theme, mode, usecommandlineargs=True, usethemeconfigfile=True):
+        super().__init__(
+            "AES File Locker",
+            theme,
+            mode,
+            usecommandlineargs=usecommandlineargs,
+            useconfigfile=usethemeconfigfile,
+        )
+
+        self.usecommandargs = tk.BooleanVar(value=True)
+        self.usethemeconfigfile = tk.BooleanVar(value=True)
+
+        self.root.resizable(False, False)
+
+        self.panedWindow = self.PanedWindow("Paned Window Test")
+
+        self.paneTop = self.panedWindow.addWindow()
+        self.paneTop.Label("AES File Locker", colspan=2)
+
+        self.descFrame = self.paneTop.addLabelFrame("Pengertian")
+        self.descFrame.Text(
+            "AESFileLocker adalah aplikasi Python yang digunakan \n"
+            "untuk mengenkripsi dan mendekripsi file teks menggunakan \n"
+            "Advanced Encryption Standard (AES). Aplikasi ini dirancang \n"
+            "bagi pengguna yang ingin menyimpan file sensitif secara aman \n"
+            "di komputer pribadi."
+        )
+
+        self.featureFrame = self.paneTop.addLabelFrame("Fitur")
+        self.featureFrame.Text(
+            "1. Enkripsi dan Dekripsi File Teks (.txt)\n"
+            "2. Antarmuka Pengguna yang Ramah\n"
+            "3. Keamanan Tingkat Tinggi\n"
+            "4. Input Kunci dari Pengguna\n"
+        )
+
+        self.systemFrame = self.paneTop.addLabelFrame("System")
+        self.systemFrame.AccentButton("Kembali", self.handleExit, widgetkwargs={"width": 45})
+
+        self.paneBottom = self.panedWindow.addWindow()
+        self.paneBottom.Label(f"All Rights Reserved \u00A9 {date.now().year}", size=10, weight="normal")
+
+        self.run()
