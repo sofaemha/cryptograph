67262418e0f9cc855c71faaac2243d3ff69b8ece
sofaemha <mail@sofa.my.id>
2025-05-09 09:49:53 +0000

    feat: Add package.json for aes-file-locker

--- system/py/library.py
+++ system/py/library.py
@@ -1,42 +1,53 @@
+import json
 import importlib.util as library_hook
 
+from system.controller.file import file
 from system.controller.sonner import sonner
 from system.py.core import system
 from system.controller.log import log
-from system.library.pycryptodome import pycryptodome
-from system.library.tkinter import tkinter
-from system.library.tkintertheme import tkintertheme
 
 
 class Library:
 
-    def check(self, module, fn):
+    def check(self):
+        f = file.read('', 'package.json', close=False)
+        data = json.loads(f.read())
+        f.close()
 
-        module_status = [self.find(name) for name in module]
-
-        for name, status in zip(fn, module_status):
-            func = globals().get(name)
-            if callable(func):
-                func(status)
-            else:
-                log.write(f"Function {name} not found")
-                return False
+        module_status = [self.find(name) for name in data["modules"]]
 
         return all(module_status)
 
     @staticmethod
     def find(module_name):
         if module_name in system.modules:
-            log.write(f"Module `{module_name!r}` already in `sys.modules`")
+            try:
+                module_version = system.modules[module_name].__version__
+            except AttributeError:
+                module_version = "unknown"
+
+            log.write(f"Module {module_name!r} (v-{module_version}) already in `sys.modules`")
             return True
         elif (spec := library_hook.find_spec(module_name)) is not None:
             module = library_hook.module_from_spec(spec)
-            system.modules[module_name] = module
-            spec.loader.exec_module(module)
-            log.write(f"Module {module_name!r} has been imported")
-            return True
+            try:
+                module_version = module.version
+                print(module_version)
+            except AttributeError:
+                module_version = "unknown"
+
+            log.write(f"Module {module_name!r} (v-{module_version}) found")
+
+            try:
+                system.modules[module_name] = module
+                spec.loader.exec_module(module)
+                log.write(f"Module {module_name!r} has been imported")
+                return True
+            except ImportError as e:
+                log.write(f"Module {module_name!r} not found: {e}")
+                return False
         else:
-            log.write(f"Can't find the `{module_name!r}` module")
+            log.write(f"Can't find {module_name!r} module")
             sonner.ubuntu_library()
             return False
 
