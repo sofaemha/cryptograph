0359224a7d638c56e386430b922695ec7b164c7f
sofaemha <mail@sofa.my.id>
2025-05-14 08:56:20 +0000

    feat(about): Improve About section layout and content

--- view/app/about.py
+++ view/app/about.py
@@ -11,17 +11,28 @@ class App(tkt.ThemedTKinterFrame):
             usecommandlineargs=usecommandlineargs,
             useconfigfile=usethemeconfigfile,
         )
+        self.root.resizable(False, False)
 
         self.usecommandargs = tk.BooleanVar(value=True)
         self.usethemeconfigfile = tk.BooleanVar(value=True)
-
-        self.root.resizable(False, False)
+        self.descFrame = None
+        self.featureFrame = None
+        self.systemFrame = None
 
         self.panedWindow = self.PanedWindow("Paned Window Test")
 
         self.paneTop = self.panedWindow.addWindow()
         self.paneTop.Label("AES File Locker", colspan=2)
 
+        self.description()
+        self.feature()
+
+        self.paneBottom = self.panedWindow.addWindow()
+        self.paneBottom.Label(f"All Rights Reserved \u00A9 {date.now().year}", size=10, weight="normal")
+
+        self.run()
+
+    def description(self):
         self.descFrame = self.paneTop.addLabelFrame("Pengertian")
         self.descFrame.Text(
             "AESFileLocker adalah aplikasi Python yang digunakan \n"
@@ -31,6 +42,7 @@ class App(tkt.ThemedTKinterFrame):
             "di komputer pribadi."
         )
 
+    def feature(self):
         self.featureFrame = self.paneTop.addLabelFrame("Fitur")
         self.featureFrame.Text(
             "1. Enkripsi dan Dekripsi File Teks (.txt)\n"
@@ -39,10 +51,6 @@ class App(tkt.ThemedTKinterFrame):
             "4. Input Kunci dari Pengguna\n"
         )
 
+    def system(self):
         self.systemFrame = self.paneTop.addLabelFrame("System")
         self.systemFrame.AccentButton("Kembali", self.handleExit, widgetkwargs={"width": 45})
-
-        self.paneBottom = self.panedWindow.addWindow()
-        self.paneBottom.Label(f"All Rights Reserved \u00A9 {date.now().year}", size=10, weight="normal")
-
-        self.run()
