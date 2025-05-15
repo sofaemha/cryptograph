cbb724611239e96302d3b21ec05d07ebd01a2629
sofaemha <mail@sofa.my.id>
2025-05-15 10:10:17 +0000

    feat(tk.py): Add support for encrypted file import

--- system/py/tk.py
+++ system/py/tk.py
@@ -4,16 +4,29 @@ import TKinterModernThemes as tkt
 from tkinter import filedialog, messagebox as alert
 
 PATH_ENCRYPT = env("PATH_ENCRYPT")
+PATH_DECRYPT = env("PATH_DECRYPT")
 
-def getfile():
-    file_path = filedialog.askopenfilename(
-        filetypes=[("Text Files", "*.txt")],
-        title="Select a Text File",
-    )
-    if file_path:
-        filename = file_path.split("/")[-1]
-        fs.copy(file_path, PATH_ENCRYPT)
+def getfile(types):
+    if types == "txt":
+        file_path = filedialog.askopenfilename(
+            filetypes=[("Text Files", "*.txt")],
+            title="Select a Text File",
+        )
+        return data(file_path, types)
+    elif types == "enc":
+        file_path = filedialog.askopenfilename(
+            filetypes=[("Encrypted Files", "*.enc")],
+            title="Select an Encrypted File",
+        )
+        return data(file_path, types)
+
+
+def data(path, types):
+    files = "a text" if types == "txt" else "an encrypted"
+    if path:
+        filename = path.split("/")[-1]
+        fs.copy(path, PATH_ENCRYPT) if types == "txt" else fs.copy(path, PATH_DECRYPT)
         alert.showinfo("System", f"File `{filename}` successfully imported into `{PATH_ENCRYPT}`.")
         return filename
     else:
-        alert.showwarning("System", "Please select a text (.txt) file!")
+        alert.showwarning("System", f"Please select {files} (.{types}) file!")
